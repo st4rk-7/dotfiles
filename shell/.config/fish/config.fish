6d1f3851e0f1a818fd062864bd13262cfb2adb77
st4rk-7 <st4rk.7@proton.me>
2025-09-18 09:15:00 +0530

    feat(fish): display nvim todos in startup

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,2 +1,30 @@
 set fish_greeting
 source $__fish_config_dir/env.fish
+
+function show_todos
+  set todo_file ~/.local/share/nvim/dooing_todos.json
+  if test -s $todo_file
+    
+    set -l TAB (printf "\t")
+    {
+      # header: todos: (left column), task, est
+      printf "%s\t%s\t%s\n" "  Todos:" "task" "est"
+      # rows: empty left col, task, est
+      jq -r '
+        .[] |
+        (.text // "-") as $text |
+        (.estimated_hours // "-") as $est |
+        ( if $est == "-" then "-" 
+          elif ($est|type == "number") then
+            if ($est % 24 == 0) then (($est/24|floor|tostring) + "d") else ($est|tostring + "h") end
+          else ($est|tostring) end
+        ) as $eststr |
+        ["", $text, $eststr] | @tsv
+      ' "$todo_file"
+    } | column -ts "$TAB"
+  end
+end
+
+if status is-interactive
+  show_todos
+end
